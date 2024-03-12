0022ad92458f8343bdbf77dd7558b64752abe838
MarynaBublyk <137985327+MarynaBublyk@users.noreply.github.com>
2024-03-12 22:24:53 +0200

    DZ-1

---
+++ index.js
@@ -0,0 +1,49 @@
+import { program } from "commander";
+import {
+  addContact,
+  listContacts,
+  getContactById,
+  removeContact,
+} from "./contacts.js";
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+// TODO: рефакторити
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contacts = await listContacts();
+      console.log(contacts);
+      break;
+
+    case "get":
+      // ... id
+      const oneContact = await getContactById(id);
+      console.log(oneContact);
+      break;
+
+    case "add":
+      const contact = await addContact(name, email, phone);
+      console.log(contact);
+      break;
+
+    case "remove":
+      // ... id
+      const deleteContact = await removeContact(id);
+      console.log(deleteContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
